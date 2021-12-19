516f924675eb2b2172924300fa97189f25d0e999
makirikov <95547054+makirikov@users.noreply.github.com>
2021-12-19 20:03:38 +0300

    Add files via upload

---
+++ Lab3/HelloApp/Form1.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            // добавление обработчика 
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+        }
+    }
+}
